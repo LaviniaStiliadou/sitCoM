90296f92df5d500941194ce0e11082cb19ae6e1c
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2019-11-27 21:18:00 +0100

    prüft die Korrektheit der Eingabe
    Leider noch Ausbesserungen notwendig....

--- app/provider/situations/parts/SituationProperties.js
+++ app/provider/situations/parts/SituationProperties.js
@@ -1,11 +1,10 @@
 import entryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
-
 import {
-  is,
-  isAny
+  is
 } from 'bpmn-js/lib/util/ModelUtil';
 
-
+// TODO: wenn es valid ist, muss man vom Objekt wieder wegklicken...
+// TODO: valid violation deletes priority value
 export default function(group, element) {
 
   // Only return an entry, if the currently selected
@@ -14,29 +13,61 @@ export default function(group, element) {
   if (is(element, 'custom:circle-red')) {
     group.entries.push(entryFactory.textField({
       id : 'violation',
-      description : 'Wenn Situation verletzt wird',
+      description : 'Wenn Situation verletzt wird, springe zu Scope ID.',
       label : 'Violation',
-      modelProperty : 'violation'
-    }));
+      modelProperty : 'violation',
+      validate: function(element, values) {
+        var violation = values.violation;
+        var errorMessageV = {};
+        if (isNaN(violation) || !isEmpty(violation)) {
+           errorMessageV.violation = "Please enter a valid scope id (number)";
+        }
+        return errorMessageV;
+       }
+    })),
     group.entries.push(entryFactory.textField({
       id : 'prioritaet',
       description : 'Prioritaet',
       label : 'prioritaet',
-      modelProperty : 'prioritaet'
-    }));
+      modelProperty : 'prioritaet',
+      validate: function(element, values) {
+        var prioritaet = values.prioritaet;
+        var errorMessageP = {};
+        if (isNaN(prioritaet) || !isEmpty(prioritaet)) {
+           errorMessageP.prioritaet = "Please enter a valid priority (number)";
+        }
+        return errorMessageP;
+   }
+  }))
   }
   if (is(element, 'custom:circle-yellow')) {
     group.entries.push(entryFactory.textField({
       id : 'violation',
-      description : 'Wenn Situation verletzt wird',
+      description : 'Wenn Situation verletzt wird, springe zu Scope ID.',
       label : 'Violation',
-      modelProperty : 'violation'
+      modelProperty : 'violation',
+      validate: function(element, values) {
+        var violation = values.violation;
+        var errorMessageV = {};
+        if (isNaN(violation) || !isEmpty(violation)) {
+           errorMessageV.violation = "Please enter a valid scope id (number)";
+           return errorMessageV;
+        }
+       }
     }));
     group.entries.push(entryFactory.textField({
       id : 'prioritaet',
       description : 'Prioritaet',
       label : 'prioritaet',
-      modelProperty : 'prioritaet'
+      modelProperty : 'prioritaet',
+      validate: function(element, values) {
+        var prioritaet = values.prioritaet;
+        var errorMessageP = {};
+        if (isNaN(prioritaet) || !isEmpty(prioritaet)) {
+           errorMessageP.prioritaet = "Please enter a valid priority (number)";
+           return errorMessageP;
+        }
+   }
     }));
   }
   if (is(element, 'custom:circle-green')) {
@@ -44,13 +75,29 @@ export default function(group, element) {
       id : 'violation',
       description : 'Wenn Situation verletzt wird',
       label : 'Violation',
-      modelProperty : 'violation'
+      modelProperty : 'violation',
+      validate: function(element, values) {
+        var violation = values.violation;
+        var errorMessageV = {};
+        if (isNaN(violation) || !isEmpty(violation)) {
+           errorMessageV.violation = "Please enter a valid scope id (number)";
+           return errorMessageV;
+        }
+   }
     }));
     group.entries.push(entryFactory.textField({
       id : 'prioritaet',
       description : 'Prioritaet',
       label : 'prioritaet',
-      modelProperty : 'prioritaet'
+      modelProperty : 'prioritaet',
+      validate: function(element, values) {
+        var prioritaet = values.prioritaet;
+        var errorMessageP = {};
+        if (isNaN(prioritaet) || !isEmpty(prioritaet)) {
+           errorMessageP.prioritaet = "Please enter a valid priority (number)";
+           return errorMessageP;
+        }
+   }
     }));
   }
   if (is(element, 'custom:rect')) {
@@ -58,13 +105,29 @@ export default function(group, element) {
       id : 'violation',
       description : 'Wenn Situation verletzt wird',
       label : 'Violation',
-      modelProperty : 'violation'
+      modelProperty : 'violation',
+      validate: function(element, values) {
+        var violation = values.violation;
+        var errorMessageV = {};
+        if (isNaN(violation) || !isEmpty(violation)) {
+           errorMessageV.violation = "Please enter a valid scope id (number)";
+           return errorMessageV;
+        }
+      }
     }));
     group.entries.push(entryFactory.textField({
       id : 'prioritaet',
       description : 'Prioritaet',
       label : 'prioritaet',
-      modelProperty : 'prioritaet'
+      modelProperty : 'prioritaet',
+      validate: function(element, values) {
+        var prioritaet = values.prioritaet;
+        var errorMessageP = {};
+        if (isNaN(prioritaet) || !isEmpty(prioritaet)) {
+           errorMessageP.prioritaet = "Please enter a valid priority (number)";
+           return errorMessageP;
+        }
+   }
     }));
   }
   if (is(element, 'bpmn:IntermediateThrowEvent')) {
@@ -72,27 +135,58 @@ export default function(group, element) {
       id : 'violation',
       description : 'Wenn Situation verletzt wird',
       label : 'Violation',
-      modelProperty : 'violation'
-    }));
-    group.entries.push(entryFactory.textField({
-      id : 'prioritaet',
-      description : 'Prioritaet',
-      label : 'prioritaet',
-      modelProperty : 'prioritaet'
-    }));
-  }
+      modelProperty : 'violation',
+      validate: function(element, values) {
+        var violation = values.violation;
+        var errorMessageV = {};
+        if (isNaN(violation) || !isEmpty(violation)) {
+           errorMessageV.violation = "Please enter a valid scope id (number)";
+        }
+        return errorMessageV;
+   }
+  }))
+  group.entries.push(entryFactory.textField({
+    id : 'prioritaet',
+    description : 'Prioritaet',
+    label : 'Prioritaet',
+    modelProperty : 'prioritaet',
+    validate: function(element, values) {
+      var prioritaet = values.prioritaet;
+      var errorMessageP = {};
+      if (isNaN(prioritaet) || !isEmpty(prioritaet)) {
+         errorMessageP.prioritaet = "Please enter a valid priority (number)";
+      }
+      return errorMessageP;
+    }
+  }))};
   if (is(element, 'bpmn:IntermediateCatchEvent')) {
     group.entries.push(entryFactory.textField({
       id : 'violation',
       description : 'Wenn Situation verletzt wird',
       label : 'Violation',
-      modelProperty : 'violation'
+      modelProperty : 'violation',
+      validate: function(element, values) {
+        var violation = values.violation;
+        var errorMessageV = {};
+        if (isNaN(violation) || !isEmpty(violation)) {
+           errorMessageV.violation = "Please enter a valid scope id (number)";
+        }
+        return errorMessageV;
+   }
     }));
     group.entries.push(entryFactory.textField({
       id : 'prioritaet',
       description : 'Prioritaet',
-      label : 'prioritaet',
-      modelProperty : 'prioritaet'
+      label : 'Prioritaet',
+      modelProperty : 'prioritaet',
+      validate: function(element, values) {
+        var prioritaet = values.prioritaet;
+        var errorMessageP = {};
+        if (isNaN(prioritaet) || !isEmpty(prioritaet)) {
+           errorMessageP.prioritaet = "Please enter a valid priority (number)";
+        }
+        return errorMessageP;
+      }
     }));
   } 
 }
