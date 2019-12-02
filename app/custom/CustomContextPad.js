5191f8532a83e587f8b3ae1649d1a27b279928ea
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2019-12-02 08:11:23 +0100

    add new icons to contextpad

--- app/custom/CustomContextPad.js
+++ app/custom/CustomContextPad.js
@@ -1,54 +1,102 @@
-import inherits from 'inherits';
+const SUITABILITY_SCORE_HIGH = 100,
+SUITABILITY_SCORE_AVERGE = 50,
+SUITABILITY_SCORE_LOW = 25;
 
-import ContextPadProvider from 'bpmn-js/lib/features/context-pad/ContextPadProvider';
+export default class CustomContextPad {
+constructor(bpmnFactory, config, contextPad, create, elementFactory, injector, translate) {
+this.bpmnFactory = bpmnFactory;
+this.create = create;
+this.elementFactory = elementFactory;
+this.translate = translate;
 
-import {
-  isAny
-} from 'bpmn-js/lib/features/modeling/util/ModelingUtil';
-
-import {
-  assign,
-  bind
-} from 'min-dash';
-
-
-export default function CustomContextPadProvider(injector, connect, translate) {
-
-  injector.invoke(ContextPadProvider, this);
-
-  var cached = bind(this.getContextPadEntries, this);
-
-  this.getContextPadEntries = function(element) {
-    var actions = cached(element);
-
-    var businessObject = element.businessObject;
-
-    function startConnect(event, element, autoActivate) {
-      connect.start(event, element, autoActivate);
-    }
-
-    if (isAny(businessObject, [ 'custom:circle-red', 'custom:circle-yellow', 'custom:circle-green', 'custom:rect'])) {
-      assign(actions, {
-        'connect': {
-          group: 'connect',
-          className: 'bpmn-icon-connection-multi',
-          title: translate('Connect using custom connection'),
-          action: {
-            click: startConnect,
-            dragstart: startConnect
-          }
-        }
-      });
-    }
-
-    return actions;
-  };
+if (config.autoPlace !== false) {
+this.autoPlace = injector.get('autoPlace', false);
 }
 
-inherits(CustomContextPadProvider, ContextPadProvider);
+contextPad.registerProvider(this);
+}
 
-CustomContextPadProvider.$inject = [
-  'injector',
-  'connect',
-  'translate'
+getContextPadEntries(element) {
+const {
+autoPlace,
+bpmnFactory,
+create,
+elementFactory,
+translate
+} = this;
+
+function appendIntermediateEvent(suitabilityScore) {
+return function(event, element) {
+  if (autoPlace) {
+    const businessObject = bpmnFactory.create('bpmn:IntermediateThrowEvent');
+
+    businessObject.suitable = suitabilityScore;
+
+    const shape = elementFactory.createShape({
+      type: 'bpmn:IntermediateThrowEvent',
+      businessObject: businessObject
+    });
+
+    autoPlace.append(element, shape);
+  } else {
+    appendIntermediateEventStart(event, element);
+  }
+}
+}
+
+function appendIntermediateEventStart(suitabilityScore) {
+return function(event) {
+  const businessObject = bpmnFactory.create('bpmn:IntermediateEvent');
+
+  businessObject.suitable = suitabilityScore;
+
+  const shape = elementFactory.createShape({
+    type: 'bpmn:IntermediateEvent',
+    businessObject: businessObject
+  });
+
+  create.start(event, shape, element);
+}
+}
+
+return {
+'append.red-circle': {
+  group: 'model',
+  className: 'bpmn-icon-intermediate-event-none red',
+  title: translate('Append red circle'),
+  action: {
+    click: appendIntermediateEvent(SUITABILITY_SCORE_LOW),
+    dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_LOW)
+  }
+},
+'append.yellow-circle': {
+  group: 'model',
+  className: 'bpmn-icon-intermediate-event-none yellow',
+  title: translate('Append yellow circle'),
+  action: {
+    click: appendIntermediateEvent(SUITABILITY_SCORE_AVERGE),
+    dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_AVERGE)
+  }
+},
+'append.green-circle': {
+  group: 'model',
+  className: 'bpmn-icon-intermediate-event-none green',
+  title: translate('Append green circle'),
+  action: {
+    click: appendIntermediateEvent(SUITABILITY_SCORE_HIGH),
+    dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_HIGH)
+  }
+}
+};
+}
+}
+
+CustomContextPad.$inject = [
+'bpmnFactory',
+'config',
+'contextPad',
+'create',
+'elementFactory',
+'injector',
+'translate'
 ];
