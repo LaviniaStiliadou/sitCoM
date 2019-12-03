c6770c7cac641143ee4ff489b712888d60b6212f
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2019-12-04 00:26:07 +0100

    remove old code
    reference to non existing html

--- app/index.js
+++ app/index.js
@@ -12,30 +12,13 @@ import {
 } from 'min-dash';
 
 import diagramXML from '../resources/newDiagram.bpmn';
-const containerEl = document.getElementById('container'),
-      qualityAssuranceEl = document.getElementById('quality-assurance'),
-      suitabilityScoreEl = document.getElementById('suitability-score'),
-      lastCheckedEl = document.getElementById('last-checked'),
-      okayEl = document.getElementById('okay'),
-      formEl = document.getElementById('form'),
-      warningEl = document.getElementById('warning');
 const HIGH_PRIORITY = 1500;
 var container = $('#js-drop-zone');
 
 var canvas = $('#js-canvas');
-// hide quality assurance if user clicks outside
-window.addEventListener('click', (event) => {
-  const { target } = event;
-
-  if (target === qualityAssuranceEl || qualityAssuranceEl.contains(target)) {
-    return;
-  }
-
-  qualityAssuranceEl.classList.add('hidden');
-});
 
 var bpmnModeler = new BpmnModeler({
-  container: canvas, containerEl,
+  container: canvas, 
   propertiesPanel: {
     parent: '#js-properties-panel'
   },
@@ -83,88 +66,6 @@ function openDiagram(xml) {
       businessObject,
       element,
       suitabilityScore;
-
-   // validate suitability score
-  function validate() {
-    const { value } = suitabilityScoreEl;
-
-    if (isNaN(value)) {
-      warningEl.classList.remove('hidden');
-      okayEl.disabled = true;
-    } else {
-      warningEl.classList.add('hidden');
-      okayEl.disabled = false;
-    }
-  }
-  // open quality assurance if user right clicks on element
-  bpmnModeler.on('element.contextmenu', HIGH_PRIORITY, (event) => {
-    event.originalEvent.preventDefault();
-    event.originalEvent.stopPropagation();
-
-    qualityAssuranceEl.classList.remove('hidden');
-
-    ({ element } = event);
-  
-    // ignore root element
-    if (!element.parent) {
-      return;
-    }
-
-    businessObject = getBusinessObject(element);
-  
-    let { suitable } = businessObject;
-    
-    suitabilityScoreEl.value = suitable ? suitable : '';
-
-    suitabilityScoreEl.focus();
-
-    analysisDetails = getExtensionElement(businessObject, 'qa:AnalysisDetails');
-
-    lastCheckedEl.textContent = analysisDetails ? analysisDetails.lastChecked : '-';
-
-    validate();
-  });
-
-  // set suitability core and last checked if user submits
-  formEl.addEventListener('submit', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    suitabilityScore = Number(suitabilityScoreEl.value);
-
-    if (isNaN(suitabilityScore)) {
-      return;
-    }
-
-    const extensionElements = businessObject.extensionElements || moddle.create('bpmn:ExtensionElements');
-
-    if (!analysisDetails) {
-      analysisDetails = moddle.create('qa:AnalysisDetails');
-
-      extensionElements.get('values').push(analysisDetails);
-    }
-
-    analysisDetails.lastChecked = new Date().toISOString();
-
-    modeling.updateProperties(element, {
-      extensionElements,
-      suitable: suitabilityScore
-    });
-
-    qualityAssuranceEl.classList.add('hidden');
-  });
-
-  // close quality assurance if user presses escape
-  formEl.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      qualityAssuranceEl.classList.add('hidden');
-    }
-  });
-
-  // validate suitability score if user inputs value
-  suitabilityScoreEl.addEventListener('input', validate);
-
-
 
   });
 }
