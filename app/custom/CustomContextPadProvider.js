5dced954a25e81657f2f8991a1331bc415ad3df5
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2019-12-11 22:46:29 +0100

    change contextpad
    TODO: für jeden Typen anders

---
+++ app/custom/CustomContextPadProvider.js
@@ -0,0 +1,52 @@
+import inherits from 'inherits';
+
+import ContextPadProvider from 'bpmn-js/lib/features/context-pad/ContextPadProvider';
+
+import {
+  isAny,
+  is
+} from 'bpmn-js/lib/features/modeling/util/ModelingUtil';
+
+import {
+  assign,
+  bind
+} from 'min-dash';
+
+
+export default function CustomContextPadProvider(injector, connect, translate) {
+  injector.invoke(ContextPadProvider, this);
+  var cached = bind(this.getContextPadEntries, this);
+
+  const _getContextPadEntries =
+    ContextPadProvider.prototype.getContextPadEntries;
+    ContextPadProvider.prototype.getContextPadEntries = function(element) {
+      var businessObject = element.businessObject;
+      const entries = _getContextPadEntries.apply(this, [element]);
+      delete entries["append.end-event"];
+      delete entries["append.intermediate-event"];
+      delete entries["append.gateway"];
+      delete entries["append.append-task"];
+      delete entries["append.text-annotation"];
+      // loescht den Schraubenschlüssel
+      delete entries["replace"];
+      // loescht den Papierkorb
+      delete entries["delete"];
+      // loescht die Arrows, also die Möglichkeit Objekte zu verbinden
+      delete entries["connect"];
+      return entries;
+    
+  }
+
+}
+
+inherits(CustomContextPadProvider, ContextPadProvider);
+
+CustomContextPadProvider.$inject = [
+
+  'injector',
+
+  'connect',
+
+  'translate'
+
+];
--- app/custom/index.js
+++ app/custom/index.js
@@ -1,10 +1,10 @@
-import CustomContextPad from './CustomContextPad';
+import CustomContextPadProvider from './CustomContextPadProvider';
 import CustomPalette from './CustomPalette';
 import CustomRenderer from './CustomRenderer';
 
 export default {
   __init__: [ 'customContextPad', 'customPalette', 'customRenderer' ],
-  customContextPad: [ 'type', CustomContextPad ],
+  customContextPad: [ 'type', CustomContextPadProvider ],
   customPalette: [ 'type', CustomPalette ],
   customRenderer: [ 'type', CustomRenderer ]
 };
