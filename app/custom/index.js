f33e800b0d5a70b29090d8db3948865c0c29eafe
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2019-12-12 23:16:42 +0100

    replace menu

--- app/custom/index.js
+++ app/custom/index.js
@@ -2,11 +2,13 @@ import CustomContextPadProvider from './CustomContextPadProvider';
 import CustomPalette from './CustomPalette';
 import CustomRenderer from './CustomRenderer';
 import CustomContextPad from './CustomContextPad';
+import ReplaceMenuProvider from './ReplaceMenuProvider';
 
 export default {
-  __init__: [ 'customContextPad', 'customPalette', 'customRenderer', 'customPad' ],
+  __init__: [ 'customContextPad', 'customPalette', 'customRenderer', 'customPad', 'replaceMenu' ],
   customContextPad: [ 'type', CustomContextPadProvider ],
   customPalette: [ 'type', CustomPalette ],
   customRenderer: [ 'type', CustomRenderer ],
-  customPad: ['type', CustomContextPad]
+  customPad: ['type', CustomContextPad],
+  replaceMenu: ['type', ReplaceMenuProvider]
 };
