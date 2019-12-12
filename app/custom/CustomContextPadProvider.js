d77c44a0f82f5976016ce5537ea10d3986dbad6c
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2019-12-12 13:45:28 +0100

    add new context pad icons, score attribute

--- app/custom/CustomContextPadProvider.js
+++ app/custom/CustomContextPadProvider.js
@@ -24,8 +24,7 @@ export default function CustomContextPadProvider(injector, connect, translate) {
 	  
 	  const entries = _getContextPadEntries.apply(this, [element]);
 	  
-	  if(element.type === "bpmn:IntermediateThrowEvent"){
-      
+	  if(element.type === "bpmn:IntermediateThrowEvent" && businessObject.suitable>0){
       delete entries["append.end-event"];
       delete entries["append.intermediate-event"];
       delete entries["append.gateway"];
@@ -47,11 +46,7 @@ export default function CustomContextPadProvider(injector, connect, translate) {
 inherits(CustomContextPadProvider, ContextPadProvider);
 
 CustomContextPadProvider.$inject = [
-
   'injector',
-
   'connect',
-
   'translate'
-
 ];
--- app/custom/index.js
+++ app/custom/index.js
@@ -1,10 +1,12 @@
 import CustomContextPadProvider from './CustomContextPadProvider';
 import CustomPalette from './CustomPalette';
 import CustomRenderer from './CustomRenderer';
+import CustomContextPad from './CustomContextPad';
 
 export default {
-  __init__: [ 'customContextPad', 'customPalette', 'customRenderer' ],
+  __init__: [ 'customContextPad', 'customPalette', 'customRenderer', 'customPad' ],
   customContextPad: [ 'type', CustomContextPadProvider ],
   customPalette: [ 'type', CustomPalette ],
-  customRenderer: [ 'type', CustomRenderer ]
+  customRenderer: [ 'type', CustomRenderer ],
+  customPad: ['type', CustomContextPad]
 };
