a3fca3822729cc73e546c246813885186796ad7a
LaviniaStiliadou <49766851+LaviniaStiliadou@users.noreply.github.com>
2020-01-03 22:06:52 +0100

    add context pad with individual changes

--- app/custom/CustomContextPad.js
+++ app/custom/CustomContextPad.js
@@ -1,94 +1,135 @@
 const SUITABILITY_SCORE_HIGH = 100,
 SUITABILITY_SCORE_AVERGE = 50,
-SUITABILITY_SCORE_LOW = 25;
+SUITABILITY_SCORE_LOW = 25,
+SUITABILITY_SCORE_OUTER_RECT = 200;
+
 
 export default class CustomContextPad {
-constructor(bpmnFactory, config, contextPad, create, elementFactory, injector, translate) {
-this.bpmnFactory = bpmnFactory;
-this.create = create;
-this.elementFactory = elementFactory;
-this.translate = translate;
+    constructor(bpmnFactory, config, contextPad, create, elementFactory, injector, translate) { 
+        this.bpmnFactory = bpmnFactory;
+        this.create = create;
+        this.elementFactory = elementFactory;
+        this.translate = translate;
 
-if (config.autoPlace !== false) {
-this.autoPlace = injector.get('autoPlace', false);
-}
+    if (config.autoPlace !== false) {
+        this.autoPlace = injector.get('autoPlace', false);
+    }
 
-contextPad.registerProvider(this);
-}
+    contextPad.registerProvider(this);
+    }
 
-getContextPadEntries(element) {
-const {
-autoPlace,
-bpmnFactory,
-create,
-elementFactory,
-translate
-} = this;
+    getContextPadEntries(element) {
+        const {
+            autoPlace,
+            bpmnFactory,
+            create,
+            elementFactory,
+            translate
+        } = this;
 
-function appendIntermediateEvent(suitabilityScore) {
-return function(event, element) {
-  if (autoPlace) {
-    const businessObject = bpmnFactory.create('bpmn:IntermediateThrowEvent');
+    function appendIntermediateEvent(suitabilityScore) {
+        return function(event, element) {
+        if (autoPlace) {
+            const businessObject = bpmnFactory.create('bpmn:IntermediateThrowEvent');
+            businessObject.suitable = suitabilityScore;
+            const shape = elementFactory.createShape({
+                type: 'bpmn:IntermediateThrowEvent',
+                businessObject: businessObject
+            });
 
-    businessObject.suitable = suitabilityScore;
+            autoPlace.append(element, shape);
+        } else {
+            appendIntermediateEventStart(event, element);
+        }}
+    }
 
-    const shape = elementFactory.createShape({
-      type: 'bpmn:IntermediateThrowEvent',
-      businessObject: businessObject
-    });
+    function appendIntermediateEventStart(suitabilityScore) {
+        return function(event) {
+            const businessObject = bpmnFactory.create('bpmn:IntermediateEvent');
+            businessObject.suitable = suitabilityScore;
+            const shape = elementFactory.createShape({
+                type: 'bpmn:IntermediateEvent',
+                businessObject: businessObject
+            });
+            create.start(event, shape, element);
+        }
+    }
 
-    autoPlace.append(element, shape);
-  } else {
-    appendIntermediateEventStart(event, element);
-  }
-}
-}
+    function appendOuterRect(suitabilityScore) {
+        return function(event, element) {
+          if (autoPlace) {
+            const businessObject = bpmnFactory.create('bpmn:SubProcess');
+            businessObject.suitable = suitabilityScore;
+            const shape = elementFactory.createShape({
+              type: 'bpmn:SubProcess',
+              businessObject: businessObject
+            });
+            autoPlace.append(element, shape);
+          } else {
+            appendOuterRectStart(event, element);
+          }
+        }
+      }
+  
+      function appendOuterRectStart(suitabilityScore) {
+        return function(event) {
+          const businessObject = bpmnFactory.create('bpmn:SubProcess');
+  
+          businessObject.suitable = suitabilityScore;
+  
+          const shape = elementFactory.createShape({
+            type: 'bpmn:SubProcess',
+            businessObject: businessObject
+          });
+          create.start(event, shape, element);
+        }
+      }
 
-function appendIntermediateEventStart(suitabilityScore) {
-return function(event) {
-  const businessObject = bpmnFactory.create('bpmn:IntermediateEvent');
-
-  businessObject.suitable = suitabilityScore;
-
-  const shape = elementFactory.createShape({
-    type: 'bpmn:IntermediateEvent',
-    businessObject: businessObject
-  });
-
-  create.start(event, shape, element);
-}
-}
-
-return {
-'append.red-circle': {
-  group: 'model',
-  //className: 'bpmn-icon-intermediate-event-none red',
-  className: 'bpmn-icon-emo-happy', 
-  title: translate('Append red circle'),
-  action: {
-    click: appendIntermediateEvent(SUITABILITY_SCORE_LOW),
-    dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_LOW)
-  }
-},
-'append.yellow-circle': {
-  group: 'model',
-  className: 'bpmn-icon-intermediate-event-none yellow',
-  title: translate('Append yellow circle'),
-  action: {
-    click: appendIntermediateEvent(SUITABILITY_SCORE_AVERGE),
-    dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_AVERGE)
-  }
-},
-'append.green-circle': {
-  group: 'model',
-  className: 'bpmn-icon-intermediate-event-none green',
-  title: translate('Append green circle'),
-  action: {
-    click: appendIntermediateEvent(SUITABILITY_SCORE_HIGH),
-    dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_HIGH)
-  }
-}
-};
+    var businessObject = element.businessObject;
+    if ((element.type === "bpmn:SubProcess") && (businessObject.suitable == 100)){
+        return {
+            'append.red-circle': {
+            group: 'model',
+            className: 'bpmn-icon-intermediate-event-none red',
+            //className: 'bpmn-icon-emo-happy', 
+            title: translate('Append red circle'),
+            action: {
+                click: appendIntermediateEvent(SUITABILITY_SCORE_LOW),
+                dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_LOW)
+            }
+        },
+            'append.yellow-circle': {
+            group: 'model',
+            className: 'bpmn-icon-intermediate-event-none yellow',
+            title: translate('Append yellow circle'),
+            action: {
+                click: appendIntermediateEvent(SUITABILITY_SCORE_AVERGE),
+                dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_AVERGE)
+            }
+        },
+            'append.green-circle': {
+            group: 'model',
+            className: 'bpmn-icon-intermediate-event-none green',
+            title: translate('Append green circle'),
+            action: {
+                click: appendIntermediateEvent(SUITABILITY_SCORE_HIGH),
+                dragstart: appendIntermediateEventStart(SUITABILITY_SCORE_HIGH)
+            }
+        }
+    };
+    } else if ((element.type === 'bpmn:SubProcess') && (businessObject.suitable == 200)){
+        return{
+            'append.outer-rect': {
+                group: 'model',
+                className: 'bpmn-icon-check-empty',
+                title: translate('Append outer rect'),
+                action: {
+                    click: appendOuterRect(SUITABILITY_SCORE_OUTER_RECT),
+                    dragstart: appendOuterRectStart(SUITABILITY_SCORE_OUTER_RECT)
+                }
+            }
+        };
+    } 
 }
 }
 
--- app/custom/CustomContextPadProvider.js
+++ app/custom/CustomContextPadProvider.js
@@ -21,7 +21,7 @@ export default function CustomContextPadProvider(injector, connect, translate) {
   const _getContextPadEntries =
     ContextPadProvider.prototype.getContextPadEntries;
     ContextPadProvider.prototype.getContextPadEntries = function(element) {
-      var businessObject = element.businessObject;
+    var businessObject = element.businessObject;
 	  
 	  const entries = _getContextPadEntries.apply(this, [element]);
 	  
@@ -33,9 +33,6 @@ export default function CustomContextPadProvider(injector, connect, translate) {
       delete entries["append.text-annotation"];
       // loescht die Arrows, also die Möglichkeit Objekte zu verbinden
       delete entries["connect"];
-      delete entries["append.red-circle"];
-      delete entries["append.yellow-circle"];
-      delete entries["append.green-circle"];
     } else if (element.type === "bpmn:SubProcess" && businessObject.suitable == 100){
       delete entries["append.end-event"];
       delete entries["append.intermediate-event"];
@@ -46,6 +43,11 @@ export default function CustomContextPadProvider(injector, connect, translate) {
       delete entries["replace"];
       // loescht die Arrows, also die Möglichkeit Objekte zu verbinden
       delete entries["connect"];
+    }else if (element.type === "bpmn:SubProcess" && businessObject.suitable == 200){
+      delete entries["append.intermediate-event"];
+      delete entries["append.append-task"];
+      // loescht den Schraubenschlüssel
+      delete entries["replace"];
     }
       return entries;
     
